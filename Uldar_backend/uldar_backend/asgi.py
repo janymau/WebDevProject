b4eb85f692bf2df5655d14ab4d53d2d2d0ce6356
aidarBARMEN <ai_aitzhanov@kbtu.kz>
2025-04-22 12:31:00 +0500

    Ready

--- Uldar_backend/uldar_backend/asgi.py
+++ Uldar_backend/uldar_backend/asgi.py
@@ -1,6 +1,4 @@
 """
-ASGI config for codehelp_backend project.
-
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
@@ -11,6 +9,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codehelp_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uldar_backend.settings')
 
 application = get_asgi_application()
